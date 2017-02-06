44e6bd09797dd3960cf63accdce0b5c491ff36e2
MatGithan <mathanrajendar@gmail.com>
2017-02-06 13:20:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
  <?php 
- echo "Hi, This is to DEMO Application Deployment from GitHub Src...<br><br>";
+ echo "Hi, I am in the version 1....<br><br>";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
