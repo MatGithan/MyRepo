462013ec5276045ea79701c31dd31856b17635d6
MatGithan <mathanrajendar@gmail.com>
2017-02-06 13:42:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
  <?php 
- echo "Hi, I am in the version 2 for demoing S2I....<br><br>";
+ echo "Hi, I am in the version 1 for traffic route demo....<br><br>";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
