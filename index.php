90f7f241cb8e7bdd1b6d37bb1287570d878a695e
MatGithan <mathanrajendar@gmail.com>
2020-05-06 18:08:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   </head>
   <body>
    
-          <h1>Welcome to an OpenShift 3.3 - Sample Application ! (Checking GitHub) </h1>
+          <h1>Welcome to an OpenShift - Demo Sample Application ! (Checking GitHub) </h1>
           <p>The purpose of this application is to demonstrate several interesting features about OpenShift.!</p>
      
           <h2>Application Information</h2>
